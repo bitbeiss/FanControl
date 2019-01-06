80083ec6c92e80ac8234286d7b5dcf8b00b41da8
Klemens Svetitsch <ksvetitsch@gmail.com>
2019-01-06 14:12:22 +0100

    removing unused variables

--- FanControl/main.cpp
+++ FanControl/main.cpp
@@ -21,62 +21,10 @@
 #include "UsartController.h"
 #include "BaudRates.h"
 
-const long LCD_COUNTER_START = 100000;
-const long LCD_COUNTER_END = 500000;
-const int small_delay = 10;
-const int large_delay = 60;
+
+const int small_delay = 10; // LEDs are updated with small delay (milliseconds)
+const int large_delay = 60; // display is updated with large delay (milliseconds)
 const int large_delay_steps = large_delay / small_delay;
-
-/*
-inline uint32_t ten_power(int power) {
-	volatile uint32_t out = 1;
-	for(int i = 0; i < power; ++i)
-	out *= 10;
-	return out;
-}
-
-void number_to_ascii(long number, char chars[], int padding_left) {
-	int i = 0;
-	
-	if (number == 0) {
-		chars[i++] = 0x30;
-	} else {
-		if (number < 0) {
-			chars[i++] = '-';
-			number = -number;
-		}
-		
-		volatile uint32_t cmp = 0;
-		volatile uint8_t quot = 0;
-		bool nr_started = false;
-		
-		for(int pot = 9; pot >= 0; pot--) {
-			cmp = ten_power(pot);
-			if ((quot = number / cmp) > 0) {
-				nr_started = true;
-				number -= cmp * quot;
-				chars[i] = quot + 0x30;
-				} else if (nr_started) {
-				chars[i] = 0x30;
-			}
-			if (nr_started)
-			++i;
-		}
-	}
-	
-	chars[i] = '\0';
-	
-	if (padding_left > i) {
-		int diff = padding_left - i;
-		for(int x = i; x >= 0; x--) {
-			chars[x + diff] = chars[x];
-		}
-		for(int x = diff-1; x >= 0; x--) {
-			chars[x] = ' ';
-		}
-	}
-}
-*/
 
 
 int main(void)
@@ -101,10 +49,7 @@ int main(void)
 	
 	int lcd_delay_update_counter = 0;
 	
-	//volatile long lcd_counter_output = LCD_COUNTER_START;
-	//char counter_output_str[7];
 	char pulse_length_us_str[5];
-	//char trigger_pin_str[2];
 	char adc_output_str[5];
 	uint16_t fan_duty_cycle = 0;
 	
@@ -137,7 +82,6 @@ int main(void)
 			lcd.SetCursorPosition(1, 0);
 			lcd.Print("Fan: ");
 			sprintf(adc_output_str, "%3d", fan_duty_cycle);
-			//number_to_ascii(fan_duty_cycle, adc_output_str, 3);
 			lcd.Print(adc_output_str);
 			lcd.Print("%");
 			
@@ -147,11 +91,6 @@ int main(void)
 			
 			
 			lcd.SetCursorPosition(2, 0);
-			//number_to_ascii(lcd_counter_output, counter_output_str);
-			//lcd.print(counter_output_str);
-			
-			//number_to_ascii(pulse_cnt>>1, counter_output_str);
-			//number_to_ascii(fan.GetPulseTimeMicroseconds(), pulse_length_us_str, 5);
 			sprintf(pulse_length_us_str, "%5ld", fan.GetFanRevolutionPeriodInMicroseconds());
 			lcd.Print(pulse_length_us_str);
 			lcd.Print("us");
@@ -162,7 +101,6 @@ int main(void)
 			serial_controller.Transmit("us");
 			serial_controller.Transmit("  ");
 			
-			//number_to_ascii(fan.GetRPM(), pulse_length_us_str, 4);
 			sprintf(pulse_length_us_str, "%4d", fan.GetFanSpeedInRoundsPerMinute());
 			lcd.Print(pulse_length_us_str);
 			lcd.Print("rpm");
@@ -174,6 +112,8 @@ int main(void)
 			
 			lcd_delay_update_counter = 0;
 			
+			
+			// SECTION receive data via serial interface
 			uint8_t receive_buf_len = serial_controller.GetReceiveBufferLength();
 			
 			if (receive_buf_len > 0) {
